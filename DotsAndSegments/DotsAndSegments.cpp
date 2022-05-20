b8edab8f17845b2f1fbbd022e2f1ad6312e2fb4b
Vyacheslav Makarov <orchara69@gmail.com>
2022-05-20 18:13:05 +0500

    quick sort added

--- DotsAndSegments/DotsAndSegments.cpp
+++ DotsAndSegments/DotsAndSegments.cpp
@@ -1,57 +1,71 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-std::vector<int32_t>::iterator FindMid(std::vector<int32_t>& v, std::vector<int32_t>::iterator beg, std::vector<int32_t>::iterator end) {
-	std::vector<int32_t>::iterator beg, end, mid;
-	int32_t ret;
-	mid = beg + (v.size());
-	ret = std::min(std::max(*beg, *end), std::max(*beg, *mid));
-	if (ret = *beg) {
-		return beg;
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+  
+int32_t FindMid(std::vector<int32_t>& v, int32_t beg, int32_t end) { 
+         int32_t ret, mid; 
+         mid =  static_cast<int64_t>(beg + end)/2; 
+         ret = std::min(std::min(std::max(v[beg], v[end]), std::max(v[beg], v[mid])), std::max(v[mid], v[end])); 
+         if (ret == v[beg]) { 
+         	return beg; 
+         } 
+         if (ret == v[end]) { 
+             return end; 
+         } 
+         if (ret == v[mid]) { 
+              return mid; 
+         } 
+         return mid;
+ } 
+  
+  
+  
+ void QuickSort(std::vector<int32_t>& v, int32_t beg, int32_t end) { 
+	if(end - beg < 1){
+	        return;
 	}
-	if (ret = *end) {
-		return end;
-	}
-	if (ret = *mid) {
-		return mid;
-	}
-}
-
-
-
-void QuickSort(std::vector<int32_t>& v, std::vector<int32_t>::iterator beg, std::vector<int32_t>::iterator end) {
-	std::vector<int32_t>::iterator mid = FindMid(v, beg, end), i = beg + 1, j = beg;
-	std::swap(*beg, *mid);
-	while (i != end) {
-		if (*beg <= *i) {
-			j++;
-			std::swap(*j, *i);
-		}
-		i++;
-	}
-	std::swap(*beg, *j);
-
-	return;
-}
-
-int main()
-{
-	int32_t n, m;
-	std::vector<int32_t> beg, end, dot;
-	/*std::cin >> n >> m;
-	for (size_t i = 0; i < n; ++i) {
-		int32_t temp;
-		std::cin >> temp;
-		beg.push_back(temp);
-		std::cin >> temp;
-		end.push_back(temp);
-	}
-	for (size_t i = 0; i < m; ++i) {
-		int32_t temp;
-		std::cin >> temp;
-		dot.push_back(temp);
-	}*/
-	beg = { 1, 3, 7, 4, 2, 6 , 8, 9, 5 };
-
-}
+         int32_t mid = FindMid(v, beg, end), i = beg + 1, j = beg; 
+         std::cout << mid << " " << v[mid] << std::endl;
+         std::swap(v[beg], v[mid]); 
+         while (i <= end) { 
+                 if (v[beg] >= v[i]) { 
+                         j++; 
+                         std::swap(v[j], v[i]); 
+                 } 
+                 i++; 
+         } 
+         std::swap(v[beg], v[j]); 
+         QuickSort(v, beg, j -1);
+         QuickSort(v, j + 1, end);
+  
+         return; 
+ } 
+  
+ int main() 
+ { 
+         //int32_t n, m; 
+         std::vector<int32_t> beg, end, dot; 
+         /*std::cin >> n >> m; 
+         for (size_t i = 0; i < n; ++i) { 
+                 int32_t temp; 
+                 std::cin >> temp; 
+                 beg.push_back(temp); 
+                 std::cin >> temp; 
+                 end.push_back(temp); 
+         } 
+         for (size_t i = 0; i < m; ++i) { 
+                 int32_t temp; 
+                 std::cin >> temp; 
+                 dot.push_back(temp); 
+         }*/ 
+         beg = { 1, 3, 7, 4, 2, 6 , 8, 9, 5 }; 
+         for(auto v : beg){
+         	std::cout << v << " ";
+         }
+         std::cout << std::endl;
+         QuickSort(beg, 0, beg.size() - 1);
+         for(auto v : beg){
+         	std::cout << v << " ";
+         }
+  
+ }
